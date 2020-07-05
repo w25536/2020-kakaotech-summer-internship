f9ee01b597c5ae2cafe6f4d723210656c1dd2775
jeonghosuh <w25536@gmail.com>
2020-07-06 02:59:37 +0900

    First commit

--- src/2020/Main.java
+++ src/2020/Main.java
@@ -1,2 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
+
+//Text Style Multiple Selections: Alt + Shift + Mouse Click
+
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.valueOf(br.readLine());
+
+
+    }
 }
