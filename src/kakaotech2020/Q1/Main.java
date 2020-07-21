4058cc21c0fc07704d8140f170cd5880da205445
jeonghosuh <w25536@gmail.com>
2020-07-21 14:32:18 +0900

    Testing.

--- src/kakaotech2020/Q1/Main.java
+++ src/kakaotech2020/Q1/Main.java
@@ -4,9 +4,6 @@ package kakaotech2020.Q1;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.sql.SQLOutput;
-import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Main {
 
